2d099ea16e2f8c5abcb256e2da90737492b7d193
MohamadMehdiDabestani <71925927+MohamadMehdiDabestani@users.noreply.github.com>
2021-09-03 16:14:41 +0430

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-const p_des_post = document.querySelectorAll(".post .content .text p");
+const p_des_post = document.querySelectorAll(".post .content .text");
 p_des_post.forEach(el => {
-    el.innerHTML = el.substring(0 , 120) + "...";
+    var title = el.querySelector("h2");
+    var img = el.querySelector("img");
+    el.innerHTML = "";
+    el.append(img);
+    el.append(title);
 });
